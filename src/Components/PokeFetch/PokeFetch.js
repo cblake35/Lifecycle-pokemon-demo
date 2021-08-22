375c431eda3a33db063576689263b59218360049
Chris Blake <c.blake@Chriss-Air.attlocal.net>
2021-08-22 18:18:19 -0400

    Completed challenge. Pokemon is now displayed after 10 seconds upon pressing the start button. The Pokemon image will remain dark until the timer runs out

--- src/Components/PokeFetch/PokeFetch.js
+++ src/Components/PokeFetch/PokeFetch.js
@@ -9,6 +9,7 @@ class PokeFetch extends Component {
       pokeInfo: '',
       pokeSprite: '',
       pokeName: '',
+      counter: 10,
     }
   }
 
@@ -16,6 +17,7 @@ class PokeFetch extends Component {
     let min = Math.ceil(1);
     let max = Math.floor(152);
     let pokeNum = Math.floor(Math.random() * (max - min) + min);
+
     fetch(`https://pokeapi.co/api/v2/pokemon/${pokeNum}`, {
       method: 'GET'
     }).then(res => res.json())
@@ -25,18 +27,63 @@ class PokeFetch extends Component {
           pokeSprite: res.sprites.front_default,
           pokeName: res.species.name,
         })
+        this.countdownTimer();
+
       })
       .catch((err) => console.log(err))
+  }
+
+  interval = () => {
+    let timeInterval = setInterval(() => {
+      this.setState({
+        counter: this.state.counter - 1,
+      })
+      if (this.state.counter <= 0) {
+        clearInterval(timeInterval)
+      }
+      console.log(this.state.counter)
+    }, 1000);
+  }
+
+  countdownTimer = () => {
+    if (this.state.counter === 10) {
+      this.interval();
+    } else {
+      this.setState({
+        counter: this.state.counter + 10,
+      })
+      this.interval();
+    }
+
+  }
+
+  componentDidMount = () => {
+
+  }
+
+  componentDidUpdate = () => {
+    if (this.state.counter === 0) {
+      document.querySelector('.pokeImg').classList.add('showPokeImg')
+    } else {
+      document.querySelector('.pokeImg').classList.remove('showPokeImg')
+    }
+  }
+
+  componentWillUnmount = () => {
+    this.setState({
+      counter: this.state.counter + 10,
+    })
   }
 
   render() {
     return (
       <div className={'wrapper'}>
         <button className={'start'} onClick={() => this.fetchPokemon()}>Start!</button>
-        <h1 className={'timer'} >Timer Display</h1>
+        {this.state.counter === 0 ? <div className='nameBlock'> <h1 className={'pokeName'}>{this.state.pokeName}</h1></div>
+          : this.state.counter >= 9 ? <div><h1 className='titleIntro'>Guess the hidden pokemon!</h1></div>
+            : <h1 className={'timer'} >{this.state.counter}s</h1>}
         <div className={'pokeWrap'}>
           <img className={'pokeImg'} src={this.state.pokeSprite} />
-          <h1 className={'pokeName'}>{this.state.pokeName}</h1>
         </div>
       </div>
     )
